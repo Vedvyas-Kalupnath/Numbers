ef999801dd20037fc8dc5e4b6d2b195669d13095
Asus <Asus@DESKTOP-D9Q8J6U>
2019-08-28 20:47:38 +0400

    Check number match

---
+++ Integers/src/Intteger.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+import java.util.Scanner;
+/**
+ * @author Asus
+ *
+ */
+public class Intteger {
+
+	/**
+	 * @param args
+	 */
+	
+	//Program to check number match
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+
+        System.out.print("Input first number: ");
+        int x = in.nextInt();
+        System.out.print("Input second number: ");
+        int y = in.nextInt();
+        System.out.print("Input third number: ");
+        int z = in.nextInt();
+
+        if (x == y && x == z)
+        {
+            System.out.println("All numbers are equal");
+        }
+        else if ((x == y) || (x == z) || (z == y))
+        {
+            System.out.println("Neither all are equal or different");
+        }
+        else
+        {
+            System.out.println("All numbers are different");
+        }
+
+	}
+
+}
